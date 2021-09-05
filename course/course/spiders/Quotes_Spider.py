c9503815654272a1a0538a9751f1e0cee166d6c0
v1ntec <leonidt668@gmail.com>
2021-09-05 18:55:15 +0300

    Connect containers

--- course/course/spiders/Quotes_Spider.py
+++ course/course/spiders/Quotes_Spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import CourseItem
 
 
 class Quotes_Spider(scrapy.Spider):
@@ -6,14 +7,14 @@ class Quotes_Spider(scrapy.Spider):
     start_urls = ['https://quotes.toscrape.com/']
 
     def parse(self, response, **kwargs):
+        items = CourseItem()
         all_div = response.xpath('//div[@class="quote"]')
         for quotes in all_div:
             text = quotes.css('span.text::text').extract()
             author = quotes.css('.author::text').extract()
             tags = quotes.css('.tag::text').extract()
+            items['title'] = text
+            items['author'] = author
+            items['tag'] = tags
 
-            yield {
-                'text': text,
-                'author': author,
-                'tag': tags
-            }
+            yield items
