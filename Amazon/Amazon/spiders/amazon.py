e8928d392c78c6ace3ad47eee4ad85067ada6ab1
v1ntec <leonidt668@gmail.com>
2021-09-11 09:40:48 +0300

    Scrape first page

--- Amazon/Amazon/spiders/amazon.py
+++ Amazon/Amazon/spiders/amazon.py
@@ -1,10 +1,22 @@
 import scrapy
-
+from ..items import AmazonItem
 
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
-    allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    page_num = 75
+    start_urls = ['https://www.amazon.com/s?i=stripbooks&rh=n%3A283155%2Cp_n_publication_date%3A1250226011&page=1&']
 
-    def parse(self, response):
-        pass
+    def parse(self, response, **kwargs):
+        all_divs = response.xpath('//div[@class="s-result-item"]').extract()
+        items = AmazonItem()
+
+        title = response.css('.a-color-base.a-text-normal::text').extract()
+        price = response.css('.a-spacing-top-small .a-price span span::text').extract()
+        author = response.css('.a-color-secondary .a-row .a-size-base+ .a-size-base::text').extract()
+        image_link = response.css('.s-image::text').extract()
+        items['title'] = title
+        items['price'] = price
+        items['author'] = author
+        items['image_url'] = image_link
+
+        yield items
